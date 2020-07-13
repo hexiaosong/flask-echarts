fb5f8a433f6758e4bc186ec01c8cb811b4aad8b4
hexiaosong <hexiaosong@zbj.com>
2020-07-13 09:27:20 +0800

    chg:flask+echarts整合.

---
+++ static/js/echarts1.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+      echarts_1();
+      function echarts_1() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echarts_1'));
+        var data = [];
+
+        option = {
+            backgroundColor: 'rgba(0,0,0,0)',
+            tooltip: {
+                trigger: 'item',
+                formatter: "{b}: <br/>{c} ({d}%)"
+            },
+            color: ['#af89d6', '#4ac7f5', '#0089ff', '#f36f8a', '#f5c847'],
+            legend: { //图例组件，颜色和名字
+                x: '70%',
+                y: 'center',
+                orient: 'vertical',
+                itemGap: 12, //图例每项之间的间隔
+                itemWidth: 10,
+                itemHeight: 10,
+                icon: 'rect',
+                data: ['行业一', '行业二', '行业三', '行业四', '行业五'],
+                textStyle: {
+                    color: [],
+                    fontStyle: 'normal',
+                    fontFamily: '微软雅黑',
+                    fontSize: 12,
+                }
+            },
+            series: [{
+                name: '行业占比',
+                type: 'pie',
+                clockwise: false, //饼图的扇区是否是顺时针排布
+                minAngle: 20, //最小的扇区角度（0 ~ 360）
+                center: ['35%', '50%'], //饼图的中心（圆心）坐标
+                radius: [50, 75], //饼图的半径
+                avoidLabelOverlap: true, ////是否启用防止标签重叠
+                itemStyle: { //图形样式
+                    normal: {
+                        borderColor: '#1e2239',
+                        borderWidth: 2,
+                    },
+                },
+                label: { //标签的位置
+                    normal: {
+                        show: true,
+                        position: 'inside', //标签的位置
+                        formatter: "{d}%",
+                        textStyle: {
+                            color: '#fff',
+                        }
+                    },
+                    emphasis: {
+                        show: true,
+                        textStyle: {
+                            fontWeight: 'bold'
+                        }
+                    }
+                },
+                data: data
+            }, {
+                name: '',
+                type: 'pie',
+                clockwise: false,
+                silent: true,
+                minAngle: 20, //最小的扇区角度（0 ~ 360）
+                center: ['35%', '50%'], //饼图的中心（圆心）坐标
+                radius: [0, 40], //饼图的半径
+                itemStyle: { //图形样式
+                    normal: {
+                        borderColor: '#1e2239',
+                        borderWidth: 1.5,
+                        opacity: 0.21,
+                    }
+                },
+                label: { //标签的位置
+                    normal: {
+                        show: false,
+                    }
+                },
+                data: data
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        //myChart.setOption(option);
+      $.ajax({
+           type: "GET",
+           url: "http://localhost:5000/test",
+           dataType: "json",
+           success: function(data){
+               option['series'][0]['data'] = data['data']
+                // 为echarts对象加载数据
+                myChart.setOption(option);
+           }
+      });
+
+
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+});
